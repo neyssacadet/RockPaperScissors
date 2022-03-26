0564d26c043ecc106056deed4fb624b5367d4259
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-03-26 18:43:43 -0500

    coded for player's input and allowed it to be printed in the console

--- external.js
+++ external.js
@@ -1,11 +1,24 @@
 //Coding for computer to play randomly.
-function computerPlay (options) {
-    return options[Math.floor(Math.random() * options.length)]; 
+function computerPlay () {
+    let options = ['rock','paper', 'scissors']; //this defines 'options'
+    let randomnumber = Math.floor(Math.random() * options.length); // randomly picks a number value '0,1,2'
+    return options[randomnumber]; //converts then returns number value into actual string value
 }
-let options = ['rock','paper', 'scissors'];
-console.log(computerPlay(options))
+console.log(computerPlay())//prints in the console
+
+//Coding for player selection 
+function playerSelection (){
+    let playerOne = window.prompt ("Choose between Rock, Paper, Scissors.");//Make sure no other option can be put in 
+    return playerOne;
+}
+console.log(playerSelection())
 
 //Coding for a single round of Rock Paper Scissors. 
-
-
+//Case where Player 1 Loses, Computer wins.
+function singleround (playerSelection, computerSelection){
+   
+    let message = 'You Lose! Paper beats Rock';
+}
+let message = 'You Lose! Paper beats Rock';
+console.log(message)
 
