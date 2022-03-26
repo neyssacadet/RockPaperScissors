6eab6b81e35eef3dc905ec1b1f097292c2af2c52
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-03-26 15:05:43 -0500

    coded for computer's play where it randomly selects between rock paper and scissors

---
+++ external.js
@@ -0,0 +1,11 @@
+//Coding for computer to play randomly.
+function computerPlay (options) {
+    return options[Math.floor(Math.random() * options.length)]; 
+}
+let options = ['rock','paper', 'scissors'];
+console.log(computerPlay(options))
+
+//Coding for a single round of Rock Paper Scissors. 
+
+
+
