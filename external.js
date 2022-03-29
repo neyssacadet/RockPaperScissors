a78b3e7a01b0c5f799cf84015fb11b28d65e906f
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-03-28 21:05:40 -0500

    added all 9 possibilites of the rock paper scissors game and included their respective alert message

--- external.js
+++ external.js
@@ -13,11 +13,45 @@ function playerPrompt(){
 }
 
 //Coding for a single round of Rock Paper Scissors. 
-//Case where Player 1 Loses, Computer wins.
 function singleround (player, computer){
+//When player inputs ROCK
     if (player == 'Rock' && computer == 'paper'){
         let message = 'You Lose! Paper beats Rock!';
         alert(message)
+    }
+    else if (player == 'Rock' && computer == 'rock' ){
+        let tieMessage = 'It is a tie';
+        alert(tieMessage)
+    }
+    else if (player == 'Rock' && computer == 'scissors'){
+        let winMessage = 'Wow, you won?';
+        alert(winMessage)
+    }
+//When player inputs PAPER
+    else if (player == 'Paper' && computer == 'rock'){
+        let WinMessage = 'WINNER!'
+        alert (WinMessage)
+    }
+    else if (player == 'Paper' && computer == 'paper'){
+        let tiedMessage = 'TIED!'
+        alert (tiedMessage)
+    }
+    else if (player == 'Paper' && computer == 'scissors'){
+        let LostMessage = 'WhatEver Major Loser!'
+        alert (LostMessage)
+    }
+//When player inputs SCISSORS
+    else if (player == 'Scissors' && computer == 'rock'){
+        let LoserMessage = 'Such a loser';
+        alert (LoserMessage)
+    }
+    else if (player == 'Scissors' && computer == 'paper'){
+        let WinnerMessage = 'Win the game, win my heart';
+        alert (WinnerMessage)
+    }
+    else if (player == 'Scissors' && computer == 'scissors'){
+        let tieddMessage = 'You better work';
+        alert (tieddMessage)
     }
 }
 console.log (singleround (playerPrompt(), computerPlay()))
