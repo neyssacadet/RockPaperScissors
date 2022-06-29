72a3d7349e227739a8137850545322219cc358df
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-06-28 19:15:55 -0500

    simplified a single round of the game

--- external.js
+++ external.js
@@ -1,3 +1,6 @@
+let WinScore = 0;
+let TieScore = 0;
+let LoseScore = 0;
 //Coding for computer to play randomly.
 function computerPlay () {
     let options = ['rock','paper', 'scissors']; //this defines 'options'
@@ -14,53 +17,23 @@ function playerPrompt(){
 
 //Coding for a single round of Rock Paper Scissors. 
 function singleround (player, computer){
-//When player inputs ROCK (3 possibilities)
-    if (player == 'Rock' && computer == 'paper'){
-        let message = 'You Lose! Paper beats Rock!';
+//When Player LOSES 
+    if (player == 'Rock' && computer == 'paper' || player == 'Paper' && computer == 'scissors' || player == 'Scissors' && computer == 'rock' ){
+        let message = 'Yikes';
         alert(message)
         return "Lose"
     }
-    else if (player == 'Rock' && computer == 'rock' ){
-        let tieMessage = 'It is a tie';
+//When it's a TIE
+    else if (player == 'Rock' && computer == 'rock' || player == 'Paper' && computer == 'paper' || player == 'Scissors' && computer == 'scissors' ){
+        let tieMessage = 'It is a tie!';
         alert(tieMessage)
         return "Tie"
     }
-    else if (player == 'Rock' && computer == 'scissors'){
-        let winMessage = 'Wow, you won?';
+//When Player WINS 
+    else {
+        let winMessage = 'Extra Point For You!';
         alert(winMessage)
         return "Win"
-    }
-//When player inputs PAPER (3 possibilities)
-    else if (player == 'Paper' && computer == 'rock'){
-        let WinMessage = 'WINNER!'
-        alert (WinMessage)
-        return "Win"
-    }
-    else if (player == 'Paper' && computer == 'paper'){
-        let tiedMessage = 'TIED!'
-        alert (tiedMessage)
-        return "Tie"
-    }
-    else if (player == 'Paper' && computer == 'scissors'){
-        let LostMessage = 'WhatEver Major Loser!'
-        alert (LostMessage)
-        return "Lose"
-    }
-//When player inputs SCISSORS (3 possibilities)
-    else if (player == 'Scissors' && computer == 'rock'){
-        let LoserMessage = 'Such a loser';
-        alert (LoserMessage)
-        return "Lose"
-    }
-    else if (player == 'Scissors' && computer == 'paper'){
-        let WinnerMessage = 'Win the game, win my heart';
-        alert (WinnerMessage)
-        return "Win"
-    }
-    else if (player == 'Scissors' && computer == 'scissors'){
-        let tieddMessage = 'You better work';
-        alert (tieddMessage)
-        return "Tie"
     }
 }
 //console.log(singleround (playerPrompt(), computerPlay()))
@@ -75,9 +48,6 @@ buttons.forEach((button) => {
 
 //Loop for a 5 round game 
 function game(button){
-let WinScore = 0;
-let TieScore = 0;
-let LoseScore = 0;
     for (let i = 0; i < 5; i++) {
         result = singleround (button, computerPlay())
         console.log(result)
