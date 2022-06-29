f313b35a1969106f0803158028f30b688fc6f814
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-06-28 20:52:40 -0500

    placed global variables which were then used for scoring in singleround function

--- external.js
+++ external.js
@@ -1,6 +1,7 @@
 let WinScore = 0;
 let TieScore = 0;
 let LoseScore = 0;
+
 //Coding for computer to play randomly.
 function computerPlay () {
     let options = ['rock','paper', 'scissors']; //this defines 'options'
@@ -10,10 +11,12 @@ function computerPlay () {
 }
 
 //Coding for player selection 
-function playerPrompt(){
-    let playerOne = ['Rock', 'Paper', 'Scissors'];//Make sure no other option can be put in but also not case
-    return playerOne;//returns the player input
-}
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+       singleround(button.id, computerPlay());
+    });
+})
 
 //Coding for a single round of Rock Paper Scissors. 
 function singleround (player, computer){
@@ -21,45 +24,40 @@ function singleround (player, computer){
     if (player == 'Rock' && computer == 'paper' || player == 'Paper' && computer == 'scissors' || player == 'Scissors' && computer == 'rock' ){
         let message = 'Yikes';
         alert(message)
-        return "Lose"
+        console.log("Lose")
+        LoseScore++;
+        console.log(LoseScore)
+        if (LoseScore ==5){
+            console.log("YOU LOST!")
+            resetGame()
+        }
     }
 //When it's a TIE
     else if (player == 'Rock' && computer == 'rock' || player == 'Paper' && computer == 'paper' || player == 'Scissors' && computer == 'scissors' ){
         let tieMessage = 'It is a tie!';
         alert(tieMessage)
-        return "Tie"
+        console.log("Tie")
+        TieScore++;
+        console.log(TieScore)
     }
 //When Player WINS 
-    else {
+    else if (player == 'Rock' && computer == 'scissors' || player == 'Paper' && computer == 'rock' || player == 'Scissors' && computer == 'paper'){
         let winMessage = 'Extra Point For You!';
         alert(winMessage)
-        return "Win"
-    }
-}
-//console.log(singleround (playerPrompt(), computerPlay()))
-
-//Adding an eventlistener to the 3 buttons
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        game(button.id);
-    });
-})
-
-//Loop for a 5 round game 
-function game(button){
-    for (let i = 0; i < 5; i++) {
-        result = singleround (button, computerPlay())
-        console.log(result)
-        if (result == "Win"){
-            WinScore++; 
-        }
-        else if (result == "Tie") {
-            TieScore++;
-        }
-        else if (result == "Lose") {
-            LoseScore++;
+        console.log("Win")
+        WinScore++; 
+        console.log(WinScore)
+        if (WinScore == 5){
+            console.log("YOU WON!")
+            resetGame()
         }
     }
-    alert (`HERE ARE THE RESULTS: Win: ${WinScore} , Tie: ${TieScore}, Lose: ${LoseScore}`)
+} 
+
+function resetGame(){
+    WinScore = 0;
+    TieScore = 0;
+    LoseScore = 0;
 }
+
+
