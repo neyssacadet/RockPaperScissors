844f603f706b4984121126434e5c2e5b33f0e204
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-06-30 07:40:13 -0500

    created function showResults to contain DOM method for showing Game Results

--- external.js
+++ external.js
@@ -2,15 +2,18 @@ let WinScore = 0;
 let TieScore = 0;
 let LoseScore = 0;
 
-
 //Coding for computer to play randomly.
-function computerPlay () {
+function computerPlay() {
     let options = ['rock','paper', 'scissors']; //this defines 'options'
     let randomnumber = Math.floor(Math.random() * options.length); // randomly picks a number value '0,1,2'
     console.log(options[randomnumber])
     return options[randomnumber]; //converts then returns number value into actual string value
 }
 
+function showResults() {
+    const gameResults = document.querySelector('#results');
+    gameResults.innerHTML = `Here are the results: Player:${WinScore} , Computer:${LoseScore}` ;
+}
 //Coding for player selection 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
@@ -19,18 +22,15 @@ buttons.forEach((button) => {
     });
 })
 
-
-
 //Coding for a single round of Rock Paper Scissors. 
-function singleround (player, computer){
+function singleround(player, computer){
 //When Player LOSES 
     if (player == 'Rock' && computer == 'paper' || player == 'Paper' && computer == 'scissors' || player == 'Scissors' && computer == 'rock' ){
         let message = 'Yikes';
         alert(message)
         console.log("Lose")
         LoseScore++;
-        const gameResults = document.querySelector('#results');
-        gameResults.innerHTML = `Here are the results: Player:${WinScore} , Computer:${LoseScore}` ;
+        showResults();
         if (LoseScore ==5){
             console.log("YOU LOST!")
             resetGame()
@@ -42,7 +42,7 @@ function singleround (player, computer){
         alert(tieMessage)
         console.log("Tie")
         TieScore++;
-        console.log(TieScore)
+       showResults();
     }
 //When Player WINS 
     else if (player == 'Rock' && computer == 'scissors' || player == 'Paper' && computer == 'rock' || player == 'Scissors' && computer == 'paper'){
@@ -50,17 +50,13 @@ function singleround (player, computer){
         alert(winMessage)
         console.log("Win")
         WinScore++; 
-        const gameResults = document.querySelector('#results');
-        gameResults.innerHTML = `Here are the results: Player:${WinScore} , Computer:${LoseScore}` ;
+        showResults();
         if (WinScore == 5){
             console.log("YOU WON!")
             resetGame()
         }
     }
 } 
-
-
-
 
 //Resetting Scores to 0 
 function resetGame(){
